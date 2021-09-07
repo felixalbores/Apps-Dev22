41f4efa95671f4895b1cec2334de0b186053ebe3
felixalbores <felixyosoresalbores@gmail.com>
2021-09-07 20:26:33 +0800

    Struct and enum

---
+++ Assignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Assignment
+{
+    public struct Student
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        public int quizScore;
+    }
+
+    public enum remark
+    {
+        Poor = 1,
+        Fair  = 2,
+        Good = 3,
+        Excellent = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student;
+
+            Console.Write("Enter id: ");
+            student.id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter firstName: ");
+            student.firstName = Console.ReadLine();
+
+            Console.Write("Enter lastName: ");
+            student.lastName = Console.ReadLine();
+
+            Console.Write("Enter quizScore: ");
+            student.quizScore = int.Parse(Console.ReadLine());
+
+
+            if (student.quizScore == (int)remark.Poor)
+                Console.WriteLine(remark.Poor);
+            else if (student.quizScore == (int)remark.Fair)
+                Console.WriteLine(remark.Fair);
+            else if (student.quizScore == (int)remark.Good)
+                Console.WriteLine(remark.Good);
+            else
+                Console.WriteLine(remark.Excellent);
+
+        }
+    }
+}
