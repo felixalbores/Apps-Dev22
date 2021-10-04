91cef2762704c0525a7d22afad850685a309cf5d
felixalbores <felixyosoresalbores@gmail.com>
2021-10-04 17:36:28 +0800

    Sumbit Button Completed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,52 +1,77 @@
 ﻿//Team Alpha
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Assignment
+namespace WinFormsApp1
 {
-    public struct Student
+    public partial class StudentForm : Form
     {
-        public int id;
-        public string firstName;
-        public string lastName;
-        public int quizScore;
-    }
-
-    public enum remark
-    {
-        Poor = 1,
-        Fair  = 2,
-        Good = 3,
-        Excellent = 4
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
+        public StudentForm()
         {
-            Student student;
+            InitializeComponent();
+        }
 
-            Console.Write("Enter id: ");
-            student.id = int.Parse(Console.ReadLine());
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-            Console.Write("Enter firstName: ");
-            student.firstName = Console.ReadLine();
+        }
 
-            Console.Write("Enter lastName: ");
-            student.lastName = Console.ReadLine();
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-            Console.Write("Enter quizScore: ");
-            student.quizScore = int.Parse(Console.ReadLine());
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sumbit_button_Click(object sender, EventArgs e)
+        {
+            int value = int.Parse(quiz_textbox.Text);
+
+            if (value == (int)remark.Poor)
+                resulting.Text = "Poor";
+            else if (value == (int)remark.Fair)
+                resulting.Text = "Fair";
+            else if (value == (int)remark.Good)
+                resulting.Text = "Good";
+            else if (value == (int)remark.Excellent)
+                resulting.Text = "Excellent";
+        }
 
 
-            if (student.quizScore == (int)remark.Poor)
-                Console.WriteLine(remark.Poor);
-            else if (student.quizScore == (int)remark.Fair)
-                Console.WriteLine(remark.Fair);
-            else if (student.quizScore == (int)remark.Good)
-                Console.WriteLine(remark.Good);
-            else
-                Console.WriteLine(remark.Excellent);
+        public enum remark
+        {
+            Poor = 1,
+            Fair = 2,
+            Good = 3,
+            Excellent = 4
+        }
 
+        public struct Student
+        {
+            public int id;
+            public string firstName;
+            public string lastName;
+            public int quizScore;
         }
     }
 }
